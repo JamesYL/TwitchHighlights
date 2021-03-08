40ab6e1ce8bc8cd1b10c669e048bfacefc538d2a
SomeStupidIdiot <james.yung.lee@gmail.com>
2021-03-08 15:57:14 -0500

    appbar

--- frontend/src/components/analysis/AnalyzeVod.tsx
+++ frontend/src/components/analysis/AnalyzeVod.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import SearchBar from "../util/SearchBar";
+import Navbar from "../util/Navbar";
 import Chart from "./SpeedsChart";
 import { useWidth, useHeight } from "../../util/getDimensions";
-
 import { getCommentsData } from "../../services/speeds";
 import { Comment } from "../../twitch_api/getComments";
 import ErrorPage from "./ErrorPage";
+import { CssBaseline } from "@material-ui/core";
 const AnalyzeVod = () => {
   const [comments, setComments] = React.useState<Comment[]>([]);
   const [isErr, setIsErr] = React.useState(false);
@@ -29,13 +29,18 @@ const AnalyzeVod = () => {
   }, [vodID]);
   return (
     <>
-      <SearchBar />
+      <CssBaseline />
+      <Navbar />
       {isErr ? (
         <ErrorPage />
       ) : (
         <>
           {commentsLoaded === null && (
-            <Chart data={comments} width={width} height={height / 3} />
+            <Chart
+              data={comments}
+              width={Math.max(1000, width)}
+              height={Math.max(height / 3, 400)}
+            />
           )}
           {commentsLoaded !== null && commentsLoaded !== -1 && (
             <div>Loaded {commentsLoaded} comments</div>
---
+++ frontend/src/components/util/Navbar.tsx
@@ -0,0 +1,64 @@
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
+import MenuIcon from "@material-ui/icons/Menu";
+import BookmarksIcon from "@material-ui/icons/Bookmarks";
+import SearchBar from "./SearchBar";
+import { Tooltip } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block",
+      },
+      marginRight: theme.spacing(2),
+    },
+    root: {
+      flexGrow: 1,
+      backgroundColor: "transparent",
+    },
+    search: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      width: "100%",
+    },
+  })
+);
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6">
+            Streamalytics
+          </Typography>
+          <div className={classes.search}>
+            <SearchBar />
+          </div>
+          <div className={classes.grow} />
+          <Tooltip title="Saved vods" aria-label="saved vods">
+            <IconButton aria-label="show saved vod analytics" color="inherit">
+              <Badge badgeContent={1} color="secondary">
+                <BookmarksIcon />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
