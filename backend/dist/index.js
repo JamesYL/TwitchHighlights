7cda0ef044b4f87c72470afbfe7fef1d720c6b5a
SomeStupidIdiot <james.yung.lee@gmail.com>
2021-04-02 14:13:43 -0400

    build

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,24 +3,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import mongoose from "mongoose";
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// import route from "./controllers/mainRoute";
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
+// import route from "./controllers/mainRoute";
+// import mongoose from "mongoose";
 dotenv_1.default.config();
 const app = express_1.default();
 if (process.env.NODE_ENV !== "production")
     app.use(cors_1.default());
 const port = process.env.PORT || 8000;
+if (process.env.NODE_ENV === "production") {
+    app.use(express_1.default.static(path_1.default.join(__dirname, "build")));
+    app.get("*", function (_, res) {
+        res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
+    });
+}
 // const databaseType =
 //   process.env.NODE_ENV === "production"
 //     ? "twitchhighlights"
 //     : process.env.NODE_ENV === "development"
 //     ? "twitchhighlightsdev"
 //     : "twtichhighlightstest";
-if (process.env.NODE_ENV === "production")
-    app.use(express_1.default.static("build"));
 // mongoose
 //   .connect(
 //     `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.8ql8a.mongodb.net/${databaseType}?retryWrites=true&w=majority`,
