ba39dbadfcbf3887078b70645d174418efd96f28
SomeStupidIdiot <james.yung.lee@gmail.com>
2021-04-02 13:56:32 -0400

    bacvkend not used for now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,37 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
-import route from "./controllers/mainRoute";
+// import route from "./controllers/mainRoute";
 import cors from "cors";
 dotenv.config();
 
 const app = express();
 if (process.env.NODE_ENV !== "production") app.use(cors());
-const port = process.env.PORT || 8000;
-const databaseType =
-  process.env.NODE_ENV === "production"
-    ? "twitchhighlights"
-    : process.env.NODE_ENV === "development"
-    ? "twitchhighlightsdev"
-    : "twtichhighlightstest";
+// const port = process.env.PORT || 8000;
+// const databaseType =
+//   process.env.NODE_ENV === "production"
+//     ? "twitchhighlights"
+//     : process.env.NODE_ENV === "development"
+//     ? "twitchhighlightsdev"
+//     : "twtichhighlightstest";
 if (process.env.NODE_ENV === "production") app.use(express.static("build"));
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.8ql8a.mongodb.net/${databaseType}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
-  .then(() => {
-    console.log("Successfully connected to MongoDB");
-    app.use("/api", route());
-    app.listen(port, () => {
-      console.log(`server running on port ${port}`);
-    });
-  })
-  .catch((err) =>
-    console.error("Failed to connect to MongoDB: " + err.message)
-  );
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.8ql8a.mongodb.net/${databaseType}?retryWrites=true&w=majority`,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//     }
+//   )
+//   .then(() => {
+//     console.log("Successfully connected to MongoDB");
+//     app.use("/api", route());
+//     app.listen(port, () => {
+//       console.log(`server running on port ${port}`);
+//     });
+//   })
+//   .catch((err) =>
+//     console.error("Failed to connect to MongoDB: " + err.message)
+//   );
